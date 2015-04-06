eb1bb2686ad1235f0817e5b028b3fb61649e5086
Rodion Aznaurov <radik_az@mail.ru>
2015-04-06 12:57:12 +0300

    First commit.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var path = 'assets/scss/**/*.scss';
+gulp.task('sass', function () {
+  gulp.src(path)
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(path, ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
